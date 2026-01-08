b15cc50d1c91fe56545606c495d8c716a81c8f7e
Sara <sarashraf881993@gmail.com>
2026-01-08 11:57:00 +0200

    Add acadmin year and subject critria

--- includes/admin-menu.php
+++ includes/admin-menu.php
@@ -7,6 +7,7 @@ add_action( 'admin_menu', 'scgs_register_admin_menu' );
 
 function scgs_register_admin_menu() {
 
+   
     add_menu_page(
         'School Grading System',
         'School Grading',
@@ -52,6 +53,25 @@ function scgs_register_admin_menu() {
         'scgs-students',
         'scgs_students_page'
     );
+    add_submenu_page(
+    'scgs-dashboard',
+    'Subject Criteria',
+    'Subject Criteria',
+    'manage_options',
+    'scgs-subject-criteria',
+    'scgs_subject_criteria_page'
+);
+
+add_submenu_page(
+    'scgs-dashboard',
+    'Academic Years',
+    'Academic Years',
+    'manage_options',
+    'scgs-academic-years',
+    'scgs_academic_years_page'
+);
+
+
 }
 
 function scgs_dashboard_page() {
@@ -73,3 +93,10 @@ function scgs_classes_page() {
 function scgs_students_page() {
     echo '<div class="wrap"><h1>Students</h1><div id="scgs-students-root"></div></div>';
 }
+function scgs_subject_criteria_page() {
+     echo '<div class="wrap"><h1>Subject Criteria</h1><div id="scgs-subject-criteria-root"></div></div>';
+}
+function scgs_academic_years_page() {
+      echo '<div class="wrap"><h1>Academic Years</h1><div id="scgs-academic-years-root"></div></div>';
+ }
+
